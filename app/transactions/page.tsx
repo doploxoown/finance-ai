d8284939a8ece4ec7f7b21f29ef85f398946b4ea
Thiago Figueira <thi.figueira@hotmail.com>
2024-11-11 21:53:02 -0300

    fix: adjust scroll on transactions page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -23,7 +23,7 @@ const TransactionPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 overflow-hidden p-6">
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
         {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
@@ -31,7 +31,7 @@ const TransactionPage = async () => {
             userCanAddTransaction={userCanAddTransactions}
           />
         </div>
-        <ScrollArea className="h-[700px] overflow-y-auto">
+        <ScrollArea className="h-full">
           <DataTable
             columns={transactionColumns}
             data={JSON.parse(JSON.stringify(transactions))}
